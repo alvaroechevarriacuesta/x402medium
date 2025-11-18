fac42ef15012399532d63de0731ef83e1744dd3a
Alvaro Echevarria Cuesta <alvaroecuesta@gmail.com>
2025-11-18 12:17:41 -0500

    generate all the routers

--- app/main.py
+++ app/main.py
@@ -1,7 +1,17 @@
 from fastapi import FastAPI
 from app.clients.medium.user import get_user_by_username
+from app.api.article import router as article_router
+from app.api.list import router as list_router
+from app.api.publication import router as publication_router
+from app.api.platform import router as platform_router
+from app.api.search import router as search_router
 
 app = FastAPI()
+app.include_router(article_router)
+app.include_router(list_router)
+app.include_router(publication_router)
+app.include_router(platform_router)
+app.include_router(search_router)
 
 
 @app.get("/")
