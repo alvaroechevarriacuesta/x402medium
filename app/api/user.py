3df2cd13f90f36b5e702373fe285c8de87fe762e
Alvaro Echevarria Cuesta <alvaroecuesta@gmail.com>
2025-11-18 12:19:47 -0500

    some fixes

--- app/api/user.py
+++ app/api/user.py
@@ -1,3 +1,11 @@
 from fastapi import APIRouter
+from app.clients.medium.user import get_user_by_username
+
 
 router = APIRouter(prefix="/user", tags=["user"])
+
+
+@router.get("/{username}")
+async def get_user(username: str):
+    """Get Medium user by username"""
+    return await get_user_by_username(username)
--- app/main.py
+++ app/main.py
@@ -5,6 +5,8 @@ from app.api.list import router as list_router
 from app.api.publication import router as publication_router
 from app.api.platform import router as platform_router
 from app.api.search import router as search_router
+from app.api.user import router as user_router
+
 
 app = FastAPI()
 app.include_router(article_router)
@@ -12,14 +14,11 @@ app.include_router(list_router)
 app.include_router(publication_router)
 app.include_router(platform_router)
 app.include_router(search_router)
-
+app.include_router(user_router)
 
 @app.get("/")
 def hello():
     return {"message": "Hello world!"}
 
 
-@app.get("/user/{username}")
-async def get_user(username: str):
-    """Get Medium user by username"""
-    return await get_user_by_username(username)
+
